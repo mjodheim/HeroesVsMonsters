39e6eeb2f34bbd9757f5ad049497e5340165e9d0
mjodheim <anthonymets.be@gmail.com>
2025-12-17 21:27:51 +0100

    program.cs updated

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,6 +2,6 @@
 using Domain.Entities.Heroes;
 
 // Création d'un plateau de jeu contenant notre héros aléatoire et les monstres
-GameBoard gameBoard = new GameBoard(HeroType.Nain,5);
+GameBoard gameBoard = new GameBoard();
 
 gameBoard.Run();
